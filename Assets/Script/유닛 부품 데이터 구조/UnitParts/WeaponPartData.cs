4ef0d129c9120cd8bfd1a29616c47aba8fea8504
KimSuengWoo97 <ksw46398693@gmail.com>
2025-12-09 17:21:35 +0900

    전투 저장

--- Assets/Script/유닛 부품 데이터 구조/UnitParts/WeaponPartData.cs
+++ Assets/Script/유닛 부품 데이터 구조/UnitParts/WeaponPartData.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "New Weapon", menuName = "Nova2/Parts/Weapon")]
-public class WeaponPartData : PartBase // [변경] PartBase 상속
+public class WeaponPartData : PartBase
 {
     [Header("Weapon Stats")]
     public int damage;
@@ -10,7 +10,11 @@ public class WeaponPartData : PartBase // [변경] PartBase 상속
     public AttackType attackType; // (기존에 정의된 Enum 사용)
 
     [Header("Prefab")]
-    public GameObject projectilePrefab; // 발사체 프리팹
+    public GameObject projectilePrefab; // 발사체 프리팹 (총알)
+
+    // --- [추가됨] 무기 자체의 3D 모델 프리팹 (칼, 총 등) ---
+    [Header("Visual")]
+    public GameObject weaponModelPrefab;
 
     private void OnEnable()
     {
